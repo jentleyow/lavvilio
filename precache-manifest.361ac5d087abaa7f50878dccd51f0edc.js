66750454d1a1eab613a08ae742e6b46b62e28674
Jentle Yow <lonestaramazed@live.com>
2019-02-10 12:02:40 +0800

    Updates

---
+++ precache-manifest.361ac5d087abaa7f50878dccd51f0edc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3a800bda0a8bf3edc4",
+    "url": "/lavvilio/static/css/main.a4eac62b.chunk.css"
+  },
+  {
+    "revision": "4d3a800bda0a8bf3edc4",
+    "url": "/lavvilio/static/js/main.4d3a800b.chunk.js"
+  },
+  {
+    "revision": "b64dffe39087fa806618",
+    "url": "/lavvilio/static/js/1.b64dffe3.chunk.js"
+  },
+  {
+    "revision": "ba5db000e353d7d9d046",
+    "url": "/lavvilio/static/js/runtime~main.ba5db000.js"
+  },
+  {
+    "revision": "6fe4be1420126c54308a06a5f5b85aa4",
+    "url": "/lavvilio/static/media/about.6fe4be14.jpg"
+  },
+  {
+    "revision": "c5df7e991a50275e889bff059873c243",
+    "url": "/lavvilio/static/media/project1.c5df7e99.jpg"
+  },
+  {
+    "revision": "20179848f108c74ab5e3de69e24b8840",
+    "url": "/lavvilio/static/media/project2.20179848.jpg"
+  },
+  {
+    "revision": "de4d1b66c94478366dc8f8bcef6f585d",
+    "url": "/lavvilio/static/media/project3.de4d1b66.jpg"
+  },
+  {
+    "revision": "6beff8cece0f2dd8066551ad993655e8",
+    "url": "/lavvilio/static/media/test1.6beff8ce.jpg"
+  },
+  {
+    "revision": "f1869492db0533763a12f3cda0d152f2",
+    "url": "/lavvilio/static/media/test2.f1869492.jpg"
+  },
+  {
+    "revision": "7961107acfda585aa86af9791d0a0278",
+    "url": "/lavvilio/static/media/test3.7961107a.jpg"
+  },
+  {
+    "revision": "072455c48f4e0fe47a044f8ec7918108",
+    "url": "/lavvilio/static/media/logo_white.072455c4.png"
+  },
+  {
+    "revision": "32ae6010269e4900fd91c1cd767e5742",
+    "url": "/lavvilio/static/media/hero-bg.32ae6010.jpg"
+  },
+  {
+    "revision": "3bb96e8ae0e51ce7c5feafd7ec67616a",
+    "url": "/lavvilio/static/media/appointment.3bb96e8a.jpg"
+  },
+  {
+    "revision": "3048fa1142f33ba5e0c6ed60234c83ba",
+    "url": "/lavvilio/index.html"
+  }
+];
